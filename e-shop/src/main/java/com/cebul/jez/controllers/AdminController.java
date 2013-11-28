3385ff864402eb4411a87eb7787b3c1d71f52706
Robert Hajdo <wwentyllo@gmail.com>
2013-11-28 13:40:15 +0100

    dodany panel admina

---
+++ e-shop/src/main/java/com/cebul/jez/controllers/AdminController.java
@@ -0,0 +1,131 @@
+package com.cebul.jez.controllers;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cebul.jez.entity.Kategoria;
+import com.cebul.jez.entity.Produkty;
+import com.cebul.jez.entity.ProduktyKupTeraz;
+import com.cebul.jez.entity.User;
+import com.cebul.jez.entity.Zdjecie;
+import com.cebul.jez.model.UserDao;
+import com.cebul.jez.service.KategorieService;
+import com.cebul.jez.service.ProduktyService;
+import com.cebul.jez.service.TestDataBaseService;
+import com.cebul.jez.service.UserService;
+import com.cebul.jez.useful.Mail;
+
+/**
+ * 
+ * @author Robert Hajdo
+ * Klasa pelniaca zadanie kontrolera w modelu MVC
+ * Jej zadaniem jest obsługa rządań z ścieżki /admin_home 
+ * Wykorzystuje mechanizm DI do wstrzykiwania zależnosći
+ */
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private KategorieService kategorieService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private TestDataBaseService dbService;
+	
+	@Autowired
+	private ProduktyService produktyService;
+	
+	@Autowired
+	private TestDataBaseService serv;
+		
+	/**
+	 * 
+	 * @param model jest to odniesienie do obiektu modelu
+	 * @return zwraca logiczną nazwę widoku
+	 */
+	@RequestMapping(value = {"/admin_home"})
+	public String admin_home(Model model, HttpSession session){
+		
+		//dbService.test1();
+		//List<Produkty> r = produktyService.getProduktyLike("drug", 19);
+		//System.out.println(r.size());
+		//Produkty pro =  r.get(0);
+		//System.out.println(pro.getNazwa());
+		
+		//serv.test1();
+		
+		//System.out.println(new Date().after(new Date()));
+		
+		List<Produkty> produkty = produktyService.getLastFourProdukt();
+		//model.addAttribute("lastFourItems", produkty);
+		session.setAttribute("lastFourItems", produkty);
+		//request.setAttribute("lastFourItems", produkty);
+		//System.out.println(produkty.size());
+		
+		
+		List<Kategoria> kat = kategorieService.getMainKategory();
+		model.addAttribute("kategoryListModel", kat);
+		session.setAttribute("kategoryList", kat);
+		
+		
+		HashMap<Integer, List<Kategoria>> podkategorie = new HashMap<Integer, List<Kategoria>>();
+		for(Kategoria k : kat)
+		{
+			podkategorie.put(k.getId(), kategorieService.getPodKategory(k.getId()) );
+		}
+		
+		
+		//userService.saveUser();
+		//System.out.println(kat.get(0).getParentKategory().);
+		
+		
+		return "admin_home";
+	}
+	@ResponseBody
+	@RequestMapping(value = "/admin_home/imag/{imageId}", method = RequestMethod.GET, produces="imag/*")
+	public void getImage(@PathVariable Integer imageId, HttpServletResponse response, HttpServletRequest request, HttpSession session, Model model) throws IOException {
+	    response.setContentType("image/jpeg");
+	    //Zdjecie requestedImage = zdjecieService.getZdjecie(2);
+	    //Zdjecie requestedImage = produktyService.getProdukt(imageId).getZdjecie();
+	    List<Produkty> produkty = (List<Produkty>) session.getAttribute("lastFourItems");
+	    //List<Produkty> produkty = (List<Produkty>) request.getAttribute("lastFourItems");
+	    Zdjecie requestedImage = null;
+	    for(Produkty p: produkty)
+	    {
+	    	if(p.getId().equals(imageId))
+	    	{
+	    		requestedImage = p.getZdjecie();
+	    		break;
+	    	}
+	    }
+	    if(requestedImage != null)
+	    {
+		    InputStream in= new ByteArrayInputStream(requestedImage.getZdjecie()); 
+		    //System.out.println("input: "+in);
+		    if (in != null) {
+		        IOUtils.copy(in, response.getOutputStream());  
+		    }
+	    }
+	}
+	
+}
---
+++ e-shop/src/main/java/com/cebul/jez/controllers/AdminPanelController.java
@@ -0,0 +1,263 @@
+package com.cebul.jez.controllers;
+
+import java.awt.image.BufferedImage;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.tools.ant.util.FileUtils;
+import org.aspectj.util.FileUtil;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cebul.jez.entity.Kategoria;
+import com.cebul.jez.entity.Produkty;
+import com.cebul.jez.entity.ProduktyKupTeraz;
+import com.cebul.jez.entity.ProduktyLicytuj;
+import com.cebul.jez.entity.User;
+import com.cebul.jez.entity.Zdjecie;
+import com.cebul.jez.service.KategorieService;
+import com.cebul.jez.service.ProduktyService;
+import com.cebul.jez.service.UserService;
+import com.cebul.jez.service.ZdjecieService;
+
+@Controller("AdminPanelController")
+public class AdminPanelController 
+{
+
+	@Autowired
+	ServletContext context;
+	
+	@Autowired
+	private KategorieService kategorieService;
+	
+	@Autowired
+	private ProduktyService produktyService;
+	
+	@Autowired
+	private ZdjecieService zdjecieService;
+	
+	
+	@RequestMapping(value = "/panel/")
+	public String getGlownyWidok()
+	{
+		return "panel";
+	}
+	@RequestMapping(value = "/panel/dodajProdukt")
+	public String dodajProduktForm()
+	{
+		return "wybierzRodzajProduktu";
+	}
+	@RequestMapping(value = "/panel/dodajProdukt/wybierzRodzajProd" , method=RequestMethod.POST)
+	public String wybranyRodzakProd(Model model, @RequestParam(value="wyborProd") String wybor, HttpSession session)
+	{
+		List<Kategoria> kat = kategorieService.getMainKategory();
+		model.addAttribute("MainKat", kat);
+		if(wybor.equals("KupTeraz"))
+		{
+			model.addAttribute("produkt", new ProduktyKupTeraz());
+			return "dodajProduktKupTeraz";
+			//return "redirect:/mojekonto/dodajProdukt/generateFormKup";
+		}else if(wybor.equals("Licytuj"))
+		{
+			model.addAttribute("produkt", new ProduktyLicytuj());
+			return "dodajProduktLicytuj";
+		}
+		return "redirect:/panel/";
+	}
+	
+	@RequestMapping(value = "/panel/dodajProdukt/generateFormKup")
+	public String generateUserForm(Model model)
+	{
+		model.addAttribute("produkt", new ProduktyKupTeraz());
+		return "dodajProduktKupTeraz";
+	}
+	@RequestMapping(value = "/panel/dodajProdukt/dodajKupTeraz", method=RequestMethod.POST)
+	public String addProduktForForm(@Valid ProduktyKupTeraz pk, BindingResult bindingResult, Model model,  HttpSession session, HttpServletRequest request) throws Exception
+	{
+		if(bindingResult.hasErrors())
+		{
+			model.addAttribute("produkt", pk);
+			model.addAttribute(bindingResult);
+			bindingResult.reject("zleeeeee");
+			System.out.println(bindingResult.getAllErrors());
+			//model.addAttribute("produkt", new ProduktyKupTeraz());
+			//return "redirect:/mojekonto/dodajProdukt/generateFormKup";
+			return "dodajProduktKupTeraz";
+		}
+		User u = (User) session.getAttribute("sessionUser");
+		pk.setUser(u);
+		boolean itsDone = produktyService.saveProduktKupTeraz(pk);
+		if(!itsDone)
+		{
+			
+			return "/panel/dodajProdukt/";
+		}
+		session.setAttribute("prod", pk);
+		session.setAttribute("firstAdd", "yes");
+		return "dodajZdjecie";
+	}
+	@RequestMapping(value = "/panel/dodajProdukt/dodajLicytuj", method=RequestMethod.POST)
+	public String addProduktForFormLicytuj(@Valid ProduktyLicytuj pl, BindingResult bindingResult, Model model,  HttpSession session, HttpServletRequest request) throws Exception
+	{
+		//pl.setDataZakonczenia(new Date());
+		if(bindingResult.hasErrors())
+		{
+			model.addAttribute("produkt", pl);
+			model.addAttribute(bindingResult);
+			bindingResult.reject("zleeeeee");
+			System.out.println(bindingResult.getAllErrors());
+			//model.addAttribute("produkt", new ProduktyKupTeraz());
+			//return "redirect:/mojekonto/dodajProdukt/generateFormKup";
+			return "dodajProduktLicytuj";
+		}
+		if(!pl.getDataZakonczenia().after(new Date()))
+		{
+			pl.setDataZakonczenia(null);
+			model.addAttribute("produkt", pl);
+			return "dodajProduktLicytuj";
+		}
+
+		User u = (User) session.getAttribute("sessionUser");
+		pl.setUser(u);
+		boolean itsDone = produktyService.saveProduktLicytuj(pl);
+		if(!itsDone)
+		{
+			return "/panel/dodajProdukt/";
+		}
+		session.setAttribute("prod", pl);
+		session.setAttribute("firstAdd", "yes");
+		return "dodajZdjecie";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/admin_home/images/{imageId}", method = RequestMethod.GET, produces="image/*")
+	public void getImage(@PathVariable Integer imageId, HttpServletResponse response) throws IOException {
+	    response.setContentType("image/jpeg");
+	    //Zdjecie requestedImage = zdjecieService.getZdjecie(2);
+	    Zdjecie requestedImage = produktyService.getProdukt(imageId).getZdjecie();
+	    
+	    InputStream in= new ByteArrayInputStream(requestedImage.getZdjecie()); 
+	    System.out.println("input: "+in);
+	    if (in != null) {
+	        IOUtils.copy(in, response.getOutputStream());  
+	    }
+	}
+	@RequestMapping(value = "/panel/dodajProdukt/dodajZdjecie", method=RequestMethod.POST)
+	public String dodajZdjecie(@RequestParam(value="image", required=false) MultipartFile image, 
+			@RequestParam(value="mainImage", required=false) String mainImage, Model model,  HttpSession session, HttpServletRequest request) throws Exception
+	{
+		
+		if(session.getAttribute("prod") != null)
+		{
+			Produkty p = (Produkty) session.getAttribute("prod");
+			try{
+				if(!image.isEmpty())
+				{
+					validateImage(image);
+					//saveImage(image);
+					//saveImageOnHardDrive(image, "H:\\");
+					Zdjecie z = returnImage(image);
+					p.addZdjecie(z);
+					if(session.getAttribute("firstAdd") != null || mainImage.equals("main") )
+					{
+						session.removeAttribute("firstAdd");
+						p.setZdjecie(z);
+					}
+					produktyService.updateProdukt(p);
+					
+				}
+			}catch(Exception e){
+					System.out.println("przed dodaniem sie wykladam");
+					//System.out.println(e.getMessage());
+					return "dodajZdjecie";
+			}
+				
+			return "dodajZdjecie";
+		}
+		return "redirect:/panel/";
+	}
+	public void validateImage(MultipartFile image) throws Exception 
+	{
+		if(!image.getContentType().equals("image/jpeg") && !image.getContentType().equals("image/png") && !image.getContentType().equals("image/x-png"))
+		{
+			throw new Exception("Plik nie ejst plikiem JPG. mam nontent= "+image.getContentType());
+		}
+	}
+	public void saveImageOnHardDrive(MultipartFile image, String path)
+	{
+		try
+		{
+			System.out.println("path= "+path);
+			File file = new File(path+image.getName());
+			org.apache.commons.io.FileUtils.writeByteArrayToFile(file, image.getBytes());
+		}catch(Exception e){
+			System.out.println("nie moge zapisac na dysku");
+		}
+	}
+	public void saveImage(MultipartFile image)
+	{
+		try{
+			Zdjecie zdj = new Zdjecie();
+			byte[] bFile  = image.getBytes();
+			zdj.setZdjecie(bFile);
+			zdjecieService.saveZdjecie(zdj);
+			
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+			System.out.println("nie udalo sie, przykro mi");
+		}
+	}
+	public Zdjecie returnImage(MultipartFile image)
+	{
+		Zdjecie zdj = new Zdjecie();
+		try{
+			byte[] bFile  = image.getBytes();
+			zdj.setZdjecie(bFile);
+			//zdjecieService.saveZdjecie(zdj);
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		return zdj;
+	}
+	@RequestMapping(value = "/panel/dodajProdukt/zakoncz/")
+	public String zakonczaDodawanieZdj(HttpSession session)
+	{
+		session.removeAttribute("prod");
+		return "redirect:/panel/";
+	}
+	@InitBinder
+    public void initBinder(WebDataBinder binder) {
+        CustomDateEditor editor = new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true);
+        binder.registerCustomEditor(Date.class, editor);
+    }
+}
